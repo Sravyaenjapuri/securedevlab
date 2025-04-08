32cf6e9be0e1dd43abe0926a7d12982a60d6e7a3
Bhanuja <sravyaenjapuri123@gmail.com>
2025-04-07 22:23:51 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -108,4 +108,4 @@ def get_lab_by_id(lab_id):
         return jsonify({'error': 'Lab not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
