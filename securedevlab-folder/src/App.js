ddba7817474a67338c533b2c4fd7f4a83ae6ff83
Bhanuja <sravyaenjapuri123@gmail.com>
2025-04-07 21:26:16 -0500

    - updated changes

--- securedevlab-folder/src/App.js
+++ securedevlab-folder/src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SQLPlayground from './components/sql_injection/playground';
+import ShopUnionLab from './components/sql_injection_helper/ShopUnionLab';
+import UnionInjectionLab from './components/sql_injection/unionInjection';
+import SQLInjectionHomepage from './components/sql_injection/homepage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import SecurityLabDashboard from './components/dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-        HOME PAGE: SECURE DEV LAB PLATFORM 
-        </p>
-      </header>
+  <Router>
+      <Routes>
+        <Route path="/" element={<SecurityLabDashboard />} />
+        <Route path="/sql_injection" element={<SQLInjectionHomepage />} />
+      <Route path="/lab/:labId" element={<SQLPlayground />} />
+        <Route path="/lab/:labId/:category" element={<UnionInjectionLab />} />
+        {/* Optional: fallback for manual entry */}
+        <Route path="/lab/union-test" element={<UnionInjectionLab />} />
+      </Routes>
+    </Router>
+    <ToastContainer />
     </div>
   );
 }
